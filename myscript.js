547113aa1b2a66d400ebcd5e74eb81897d2cceb2
Opeoluwaaa <66249968+Opeoluwaaa@users.noreply.github.com>
2020-06-06 11:20:40 +0100

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,7 @@
+//declaring variables
+var fullName = 'Bilewu Samuel Opeoluwa';
+var ID = 'HNG-03741';
+var language = 'javascript';
+
+//output
+console.log('Hello World, this is ' + fullName + ' with HNGi7 ID ' + ID + ' using ' + language + ' for stage 2 task.');
